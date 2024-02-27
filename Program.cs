3e413387b678f9340e35443967b3cd45d801e48a
DESKTOP-B8DCFDH\mohan <mohanad-yousif@outlook.com>
2024-02-27 15:16:53 +0100

    Detta är den enklaste lösningen jag kan komma med.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Just for test");
+            Console.WriteLine("Pleas Enter new Employee's name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Pleas Enter new Employee's salary: ");
+            string salary = Console.ReadLine();
+
+            Console.WriteLine("The new Employee's name is " + name + " and his salary is " + salary + " sek per month");
         }
     }
 }
